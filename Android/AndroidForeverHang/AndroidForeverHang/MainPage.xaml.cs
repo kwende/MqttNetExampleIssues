7100f65381d07e4d6115c41aaa510fce37a8bb5b
Ben Rush <brush@ocuvera.com>
2022-05-23 16:41:50 -0500

    Duplicate error

---
+++ Android/AndroidForeverHang/AndroidForeverHang/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AndroidForeverHang
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    IMqttClient client = new MqttFactory().CreateMqttClient();
+                    var clientOptions = new MqttClientOptionsBuilder()
+                           .WithClientId("client")
+                           .WithTcpServer("172.17.5.8", 1234)
+                           .WithKeepAlivePeriod(TimeSpan.FromSeconds(5))
+                           .WithCommunicationTimeout(TimeSpan.FromSeconds(5))
+                           .Build();
+                    await client.ConnectAsync(clientOptions);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex); 
+                }
+            }); 
+        }
+    }
+}
